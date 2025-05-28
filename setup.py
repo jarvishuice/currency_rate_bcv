e614cd0822d3ed56344fc71fbf390347fb8bf034
jarvishuice <jarvis.realg@gmail.com>
2025-05-28 12:51:02 -0400

    re

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ setup(
     version="0.1.0",                   # Versión inicial
     author="Jarvis Gabriel Huice Padron",
     author_email="jarvis.realg@gmail.com",
-    description="Una breve descripción de tu librería",
+    description="Librería para obtener la tasa oficial del Euro y Dólar publicada por el Banco Central de Venezuela (BCV).",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/tuusuario/mi_libreria ",  # Repo público
-    packages=find_packages(),          # Encuentra automáticamente los paquetes
+    url="https://github.com/jarvishuice/currency_rate_bcv ",  # Repo público
+    packages=find_packages(),
+    install_requires=[
+        "requests>=2.32.3",
+        "bs4>=0.0.2",
+    ],          # Encuentra automáticamente los paquetes
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
